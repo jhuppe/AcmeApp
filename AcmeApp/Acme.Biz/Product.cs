2a7c95e002c6b4547dba4dfb11e0743d96bee656
jerhup <jeremy.huppe@extendhealth.com>
2018-03-12 13:47:48 -0600

    First commit

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,117 @@
+﻿using Acme.Common;
+using static Acme.Common.LoggingService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        //Constructors
+        public Product()
+        {
+            //var colorOptions = new string[4];
+            //colorOptions[0] = "Red";
+            //colorOptions[0] = "Espresso";
+            //colorOptions[0] = "White";
+            //colorOptions[0] = "Navy";
+            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+
+            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
+
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+            }
+            foreach (var color in colorOptions)
+            {
+                Console.WriteLine($"The color is {color}");
+            }
+        }
+
+        #region Properties
+        public DateTime? AvailabilityDate { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProductId { get; set; }
+
+        private string productName;
+        public string ProductName
+        {
+            get {
+                var formattedValue = productName?.Trim();
+                return formattedValue;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    ValidationMessage = "Product Name must be at least 3 characters";
+                }
+                else if (value.Length > 20)
+                {
+                    ValidationMessage = "Product Name cannot be more than 20 characters";
+
+                }
+                else
+                {
+                    productName = value;
+
+                }
+            }
+        }
+
+        private Vendor productVendor;
+        public Vendor ProductVendor
+        {
+            get {
+                if (productVendor == null)
+                {
+                    productVendor = new Vendor();
+                }
+                return productVendor;
+            }
+            set { productVendor = value; }
+        }
+
+        public string ValidationMessage { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Calculates the suggested retail price
+        /// </summary>
+        /// <param name="markupPercent">Percent used to mark up the cost.</param>
+        /// <returns></returns>
+        public OperationalResultDecimal CalculateSuggestedPrice(decimal markupPercent)
+        {
+            var message = "";
+            if (markupPercent <= 0m)
+            {
+                message = "Invalid markup percentage";
+            }
+            else if (markupPercent < 10)
+            {
+                message = "Below recommended markup percentage";
+            }
+            var value = this.Cost + (this.Cost * markupPercent / 100);
+
+            var operationalResult = new OperationalResultDecimal(value, message);
+            return operationalResult;
+        }
+
+        public override string ToString()
+        {
+            return this.ProductName + " (" + this.ProductId + ")";
+        }
+    }
+}
---
+++ AcmeApp/Acme.Common/OperationResult.cs
@@ -0,0 +1,39 @@
+﻿namespace Acme.Common
+{
+    /// <summary>
+    /// Provides a success flag and message 
+    /// useful as a method return type.
+    /// </summary>
+    public class OperationResult<T>
+    {
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(T result, string message) : this()
+        {
+            this.Result = result;
+            this.Message = message;
+        }
+
+        public T Result { get; set; }
+        public string Message { get; set; }
+    }
+    public class OperationalResultDecimal
+    {
+        public OperationalResultDecimal ()
+        {
+
+        }
+
+        public OperationalResultDecimal(decimal result, string message) : this()
+        {
+            this.Result = result;
+            this.Message = message;
+        }
+        public decimal Result { get; set; }
+        public string Message { get; set; }
+    }
+
+
+}
